02cf745b09465392e8031463c21defac7e14feae
izhbannikov <zhbannikov.ilya@gmail.com>
2016-11-01 22:37:57 +0300

    PCA: serious bug fixed.

--- rqt.R
+++ rqt.R
@@ -6,6 +6,7 @@ library(homals)
 library(Matrix)
 library(CompQuadForm)
 library(parallel)
+library(pls)
 
 ## Empirical null distribution for Q3 test
 null.dist.Q3 <- read.table("~/Projects/rqt/n.log10.minp.1e09.txt",header=T)
@@ -38,14 +39,14 @@ get.maf <-function(vec){
 ## a: shape parameter for GM method                                                          
 ###############################################################################################
 
-QTest.one<-function(y,covadat=NULL,newgeno,STT=0.2,weight=FALSE, cumvar.threshold=90, eig.threshold=0.0005){
+QTest.one<-function(y,covadat=NULL,newgeno,STT=0.2,weight=FALSE, cumvar.threshold=90, reg.family="binomial"){
   ### DEBUG ###
-  y <- matrix(ncol=1,data[,1])
-  newgeno <- data[,2:dim(data)[2]]
-  covadat <- NULL
-  STT <- 0.2
-  weight <- FALSE
-  
+  #y <- matrix(ncol=1,data[,1])
+  #newgeno <- data[,2:dim(data)[2]]
+  #covadat <- NULL
+  #STT <- 0.2
+  #weight <- FALSE
+  #cumvar.threshold <- 90
   ####
   ## If covariates exist
   #if(length(covadat)!=0){
@@ -65,18 +66,39 @@ QTest.one<-function(y,covadat=NULL,newgeno,STT=0.2,weight=FALSE, cumvar.threshol
   cumvar <- cumsum(variance)
   eig.decathlon2.active <- data.frame(eig = eig, variance = variance, cumvariance = cumvar)
   #head(eig.decathlon2.active)
+  
+  ######### Filtering by threshold ##############
+  S <- res.pca$x[,which(eig.decathlon2.active$cumvar <= cumvar.threshold)] %*% t(res.pca$rotation[,which(eig.decathlon2.active$cumvar <= cumvar.threshold)])
+  
+  ######### And add the center (and re-scale) back to data ###########
+  if(res.pca$scale != FALSE){
+    S <- scale(S, center = FALSE , scale=1/res.pca$scale)
+  }
+  if(res.pca$center != FALSE){
+    S <- scale(S, center = -1 * res.pca$center, scale=FALSE)
+  }
+  
   #### End of calculating PCA ####
   
+  #### PLS ####
+  #res.plsr <- plsr(y ~ ., data = newgeno, validation = "LOO")
+  # TODO TODO TODO
+  #############
+  
   #### Regression after PCA ####
-  S <- try(as.matrix(res.pca$x[,which(eig.decathlon2.active$cumvar <= cumvar.threshold)]),TRUE)
-  t.fit <- try(glm(y ~ .,data=data.frame(S), family = poisson(link=log)),TRUE)
-  fit <- try(glm(y ~ .,data=data.frame(S), family = binomial(link = log), start=coef(t.fit)),TRUE)
+  
+  if(reg.family == "binomial") {
+    #t.fit <- try(glm(y ~ ., data=data.frame(S), family = poisson(link = log)),TRUE) #Poisson first
+    fit <- try(glm(y ~ ., data=data.frame(S), family = binomial(link=logit)),TRUE)
+  } else if(reg.family == "gaussian") {
+    fit <- try(glm(y ~ ., data=data.frame(S), family = gaussian),TRUE)
+  }
   na.S <- try(which(is.na(coef(fit)[-1]) == TRUE),TRUE)
   
   #### Calculating statistics and p-values ####
-  if(length(na.S)>0){
+  if(length(na.S) > 0){
     S <- try(as.matrix(S[,-na.S]),TRUE)
-    fit <- try(glm(resid~.,data=data.frame(S)),TRUE)
+    fit <- try(glm(y ~ . ,data=data.frame(S)),TRUE)
   }
   coef <- try(coef(summary(fit))[-1,1:2],TRUE)
   
@@ -84,41 +106,51 @@ QTest.one<-function(y,covadat=NULL,newgeno,STT=0.2,weight=FALSE, cumvar.threshol
   if(mode(fit)=="character"){length(coef)<-0}
   
   if(length(coef)!=0){
+    
     if(length(coef)!=2){beta1<-coef[,1];se1<-coef[,2]}
     if(length(coef)==2){beta1<-coef[1];se1<-coef[2]}
-    SS<-cbind(1,S); n<-length(y)
-    vv<-vcov(fit)[-1,-1]
-    alpha<-(1/(se1^2))/sum(1/(se1^2))
-    #vv<-solve(t(SS)%*%SS)[-1,-1]*var(y)*(n-1)/n;alpha<-(1/(se1^2))
+    SS <- cbind(1,S)
+    n <- length(y)
+    vv <- vcov(fit)[-1,-1]
+    alpha <- (1/(se1^2))/sum(1/(se1^2))
     
     ##QTest1##
-    if(weight==FALSE){
-      var.pool0<-t(alpha)%*%vv%*%alpha
-      beta.pool0<-t(alpha)%*%beta1
-      z.score0<-beta.pool0/sqrt(var.pool0)
-      Q1<-z.score0^2
-      p.Q1<-pchisq(Q1,df=1,lower.tail=FALSE)
+    if(weight == FALSE){
+      var.pool0 <- t(alpha) %*% vv %*% alpha
+      beta.pool0 <- t(alpha) %*% beta1
+      z.score0 <- beta.pool0/sqrt(var.pool0)
+      Q1 <- z.score0^2
+      p.Q1 <- pchisq(Q1, df=1, lower.tail=FALSE)
     }
-    if(weight==TRUE){
-      maf.S<-apply(S,2,function(v)mean(na.omit(v))/2)
-      #w.S0<-1/sqrt(maf.S*(1-maf.S));
-      w.S0<-qbeta(maf.S,1,25,lower.tail=F)
-      WS<-diag(w.S0);if(length(beta1)==1){WS<-w.S0}
-      var.pool<-t(alpha)%*%WS%*%vv%*%WS%*%alpha
-      beta.pool<-t(alpha)%*%WS%*%beta1
-      z.score<-beta.pool/sqrt(var.pool)
-      Q1<-z.score^2
-      p.Q1<-pchisq(Q1,df=1,lower.tail=FALSE)
+    
+    if(weight == TRUE){
+      maf.S <- apply(S,2,function(v)mean(na.omit(v))/2)
+      w.S0 <- qbeta(maf.S,1,25,lower.tail=F)
+      WS <- diag(w.S0)
+      if(length(beta1)==1){
+        WS <- w.S0
+      }
+      var.pool <- t(alpha) %*% WS %*% vv %*% WS %*% alpha
+      beta.pool <- t(alpha) %*% WS %*% beta1
+      z.score <- beta.pool/sqrt(var.pool)
+      Q1 <- z.score^2
+      p.Q1 <- pchisq(Q1, df=1, lower.tail=FALSE)
     }
     
     ## QTest2 ##
-    Q2.eigen<-eigen(vv);U2<-Q2.eigen$vectors; l2<-Q2.eigen$values;na.l2<-which(l2/mean(l2)<0.01)
-    if(length(na.l2)>0){l2<-l2[-na.l2];U2<-U2[,-na.l2]}
-    p2<-pchisq((t(U2)%*%beta1)^2/l2,df=1,lower.tail=FALSE)
-    a<-get.a(length(p2),STT)
-    q2<-2*(qgamma(p2,a,1,lower.tail=FALSE))
-    Q2<-sum(q2)
-    p.Q2<-pchisq(Q2,df=2*a*length(q2),lower.tail=FALSE)
+    Q2.eigen <- eigen(vv)
+    U2 <- Q2.eigen$vectors
+    l2 <- Q2.eigen$values
+    na.l2 <- which(l2/mean(l2) < 0.01)
+    if(length(na.l2) > 0){
+      l2 <- l2[-na.l2]
+      U2 <- U2[,-na.l2]
+    }
+    p2 <- pchisq((t(U2) %*% beta1)^2/l2, df=1, lower.tail=FALSE)
+    a <- get.a(length(p2), STT)
+    q2 <- 2*(qgamma(p2, a, 1, lower.tail=FALSE))
+    Q2 <- sum(q2)
+    p.Q2 <- pchisq(Q2, df=2*a*length(q2), lower.tail=FALSE)
     
     
     ##QTest3 ##
@@ -167,7 +199,9 @@ QTest.one<-function(y,covadat=NULL,newgeno,STT=0.2,weight=FALSE, cumvar.threshol
     names(rslt)<-c("Qstatistic", "p.value")	
   }
   
-  if(length(coef)==0){rslt<-NA}
+  if(length(coef)==0) { 
+    rslt<-NA
+  }
   options (warn=-1)
   return(rslt)	
 }
