21e64369a35848940e5ab0dd0eadea7af30f7b02
izhbannikov <zhbannikov.ilya@gmail.com>
2016-11-11 04:59:04 +0300

    Minor bug fixes.

--- R/rQTest.R
+++ R/rQTest.R
@@ -14,11 +14,11 @@ rQTest <- function(pheno, geno, perm=0, STT=0.2,weight=FALSE, cumvar.threshold=9
   num.cores <- detectCores(all.tests = FALSE, logical = TRUE)
   
   if(perm==0){
-    rslt <- try(QTest.one(y=pheno,covadat=NULL,newgeno=geno, STT=STT, cumvar.threshold=cumvar.threshold, reg.family=reg.family),TRUE)
+    rslt <- try(QTest.one(y=pheno,covadat=NULL,newgeno=geno, STT=STT, cumvar.threshold=cumvar.threshold, reg.family=reg.family, method=method),TRUE)
   } else {
     rsltPP <- do.call(rbind, lapply(1:perm, function(k){
                                                           yP <- pheno[sample(1:length(pheno),length(pheno),replace=F)]
-                                                          t.res <- QTest.one(y=yP,covadat=NULL, newgeno=geno,STT=STT,weight=weight, cumvar.threshold=cumvar.threshold, reg.family=reg.family)
+                                                          t.res <- QTest.one(y=yP,covadat=NULL, newgeno=geno,STT=STT,weight=weight, cumvar.threshold=cumvar.threshold, reg.family=reg.family, method=method)
                                                           if(is.na(t.res)) {
                                                             t.res <- list( data.frame(Q1=NA, Q2=NA, Q3=NA), data.frame(p.Q1=1,p.Q2=1,p.Q3=1) )
                                                             names(rslt)<-c("Qstatistic", "p.value")	
@@ -29,7 +29,7 @@ rQTest <- function(pheno, geno, perm=0, STT=0.2,weight=FALSE, cumvar.threshold=9
                                               })
                       )
     
-    rslt0 <- try(as.numeric(QTest.one(y=pheno, covadat=NULL, newgeno=geno, STT=STT, weight=weight, cumvar.threshold=cumvar.threshold, reg.family=reg.family)$p.value),TRUE)
+    rslt0 <- try(as.numeric(QTest.one(y=pheno, covadat=NULL, newgeno=geno, STT=STT, weight=weight, cumvar.threshold=cumvar.threshold, reg.family=reg.family, method=method)$p.value),TRUE)
     if(!is.na(rslt0)) {
       
       rslt <- data.frame(p.Q1 = (length(which(rsltPP[,1] < rslt0[1])) + 1) / (perm + 1),
