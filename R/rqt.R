035c2717c1597088b1aad1af67de69d4c2b35db4
izhbannikov <zhbannikov.ilya@gmail.com>
2016-12-15 19:19:12 -0500

    Bug with unexpected termination fixed.

--- R/rqt.R
+++ R/rqt.R
@@ -8,14 +8,13 @@
 #' @return a TODO
 get.a <- function(L,STT) {
     aa <- diff <- seq(0,1,length=200)
-    for(i in 1:length(aa)){
-        diff[i] <- abs(get.stt(L,aa[i], STT) - STT)
-    }
+    diff <- sapply(1:length(aa), function(i) { abs(get.stt(L,aa[i], STT) - STT) } )
     return(aa[which.min(diff)])
 }
 
-get.stt<-function(L,a,STT){
-    1-pgamma(L*qgamma(1-STT,a,1),L*a,1)
+get.stt <- function(L,a,STT){
+    ans <- 1-pgamma(L*qgamma(1-STT,a,1),L*a,1)
+    return(ans)
 }
 
 
@@ -148,7 +147,7 @@ QTest.one <- function(phenotype, genotype, covariates, STT=0.2, weight=FALSE,
                     p.Q1 <- pchisq(Q1, df=1, lower.tail=FALSE)
                 }
           
-                ## QTest2 ##
+                ### QTest2 ###
                 Q2.eigen <- eigen(vv)
                 U2 <- Q2.eigen$vectors
                 l2 <- Q2.eigen$values
@@ -157,50 +156,55 @@ QTest.one <- function(phenotype, genotype, covariates, STT=0.2, weight=FALSE,
                     l2 <- l2[-na.l2]
                     U2 <- U2[,-na.l2]
                 }
-          
+                
                 p2 <- pchisq((t(U2) %*% beta1)^2/l2, df=1, lower.tail=FALSE)
                 a <- get.a(length(p2), STT)
                 q2 <- 2*(qgamma(p2, a, 1, lower.tail=FALSE))
                 Q2 <- sum(q2)
                 p.Q2 <- pchisq(Q2, df=2*a*length(q2), lower.tail=FALSE)
-          
-                ##QTest3 ##
+                
+                ######################## QTest3 ###########################
+                
                 if(weight==FALSE){
                     cov.beta <- c(t(alpha) %*% vv)
                     b.star <- beta1 - beta.pool0*cov.beta/var.pool0[1]
                     vv.star <- vv - (cov.beta%*%t(cov.beta))/var.pool0[1]
-                }
-                if(weight==TRUE){
-                    w.vv<-WS%*%vv%*%WS
+                } else {
+                    w.vv<-WS %*% vv %*% WS
                     c(t(alpha)%*%w.vv)->cov.beta
-                    b.star<-WS%*%beta1-beta.pool*cov.beta/var.pool[1]
-                    vv.star<-w.vv-(cov.beta%*%t(cov.beta))/var.pool[1]
+                    b.star <- WS %*% beta1 - beta.pool*cov.beta/var.pool[1]
+                    vv.star <- w.vv-(cov.beta%*%t(cov.beta))/var.pool[1]
                 }
-    
-                if(length(beta1)!=1){
-                    Q3.eigen<-eigen(vv.star)
-                    U3<-Q3.eigen$vectors
-                    l3<-Q3.eigen$values
-                    na.l3<-which(l3/mean(l3)<0.001)
-                    if(length(na.l3)>0){l3<-l3[-na.l3];U3<-U3[,-na.l3]}
-                    L3<-try(diag(l3),TRUE);if(length(l3)==1){L3<-l3}
-                    q2.proj<-(t(U3)%*%b.star)^2/l3; 
-                    p2.1<-pchisq(q2.proj,df=1,lower.tail=FALSE)
+                
+                if(length(beta1) !=1 ) {
+                    Q3.eigen <- eigen(vv.star)
+                    U3 <- Q3.eigen$vectors
+                    l3 <- Q3.eigen$values
+                    na.l3 <- which(l3/mean(l3) < 0.001)
+                    
+                    if(length(na.l3) > 0) {l3<-l3[-na.l3];U3<-U3[,-na.l3]}
+                    
+                    #L3 <- try(diag(l3), TRUE)
+                    #if(length(l3)==1){L3<-l3}
+                    
+                    q2.proj <- (t(U3) %*% b.star)^2/l3
+                    p2.1 <- pchisq(q2.proj, df=1, lower.tail=FALSE)
                     a <- get.a(length(p2.1),STT)
-                    Q2.proj<-sum(2*qgamma(p2.1,a,1,lower.tail=FALSE))
-                    p.Q2.proj<-pchisq(Q2.proj,df=2*a*length(l3),
+                    Q2.proj <- sum(2*qgamma(p2.1,a,1,lower.tail=FALSE))
+                    p.Q2.proj <- pchisq(Q2.proj,df=2*a*length(l3),
                         lower.tail=FALSE)
-                    Q2.1<-qchisq(p.Q2.proj,df=1,lower.tail=FALSE)
-      
+                    if(p.Q2.proj == 0) {p.Q2.proj <- 1e-8}
+                    Q2.1 <- qchisq(p.Q2.proj,df=1,lower.tail=FALSE)
+                    #print(paste(Q2.1, p2.1, Q2.proj, p.Q2.proj, a))
                     pi0 <- seq(0,1,by=0.1)
                     p.Q3.can <- Q3 <- rep(1,11)
-                    p.Q3.can[1]<-p.Q2.proj
+                    p.Q3.can[1] <- p.Q2.proj
                     Q3[1] <- Q2.1
-                    p.Q3.can[11]<-p.Q1
+                    p.Q3.can[11] <- p.Q1
                     Q3[11] <- Q1
                     
                     for(h in 2:10){
-                        Q3[h] <- pi0[h]*Q1+(1-pi0[h])*Q2.1
+                        Q3[h] <- pi0[h]*Q1 + (1-pi0[h])*Q2.1
                         p.Q3.can[h] <- davies(Q3[h],
                             c(pi0[h],(1-pi0[h])),
                             c(1,1))$Qq
@@ -214,13 +218,13 @@ QTest.one <- function(phenotype, genotype, covariates, STT=0.2, weight=FALSE,
                                 (1-pi0[h])),c(1,1))[1]
                         }
                     }
-      
+                  
                     Q3final <- Q3[which.min(p.Q3.can)]
                     p.Q3 <- (sum(null.dist.Q3[null.dist.Q3[,1] > 
                         -log10(min(p.Q3.can)),2])+1) / 
                         (sum(null.dist.Q3[,2])+1)
                 }
-    
+                
                 if(length(beta1)==1){p.Q3<-p.Q1; Q3final<-Q1}
           
                 rslt <- list( data.frame(Q1, Q2, Q3=Q3final), 
