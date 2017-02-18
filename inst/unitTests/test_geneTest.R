0b755f466755734e96de83a416b2110928b9711f
izhbannikov <zhbannikov.ilya@gmail.com>
2017-02-18 14:09:53 -0500

    Code refractoring, version incremented

--- inst/unitTests/test_geneTest.R
+++ inst/unitTests/test_geneTest.R
@@ -1,4 +1,4 @@
-test_rQTest <- function() {
+test_geneTest <- function() {
     # Loading data and constructing the objects #
     data <- data.matrix(read.table(system.file("extdata/test.bin1.dat",
                                              package="rqt"), header=TRUE))
@@ -6,9 +6,9 @@ test_rQTest <- function() {
     geno <- data[, 2:dim(data)[2]]
     colnames(geno) <- paste(seq(1, dim(geno)[2]))
     geno.obj <- SummarizedExperiment(geno)
-    obj <- rqtClass(phenotype=pheno, genotype=geno.obj)
+    obj <- rqt(phenotype=pheno, genotype=geno.obj)
     # Analysis #
-    res <- rQTest(obj, method="pca", out.type = "D")
+    res <- geneTest(obj, method="pca", out.type = "D")
     checkEqualsNumeric(results(res)$Qstatistic$Q1, 0.3798991, tolerance=1.0e-4)
     checkEqualsNumeric(results(res)$Qstatistic$Q2, 13.83897, tolerance=1.0e-4)
     checkEqualsNumeric(results(res)$Qstatistic$Q3, 0.3798991, tolerance=1.0e-4)
